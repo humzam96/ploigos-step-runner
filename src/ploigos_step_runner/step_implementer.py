13d3fc2ae71c91267428a28e4a9ed228fec9c10c
Humza Mohammad <humzam96@icloud.com>
2021-05-05 19:35:14 -0400

    Fixing path again

--- src/ploigos_step_runner/step_implementer.py
+++ src/ploigos_step_runner/step_implementer.py
@@ -446,7 +446,7 @@ class StepImplementer(ABC):  # pylint: disable=too-many-instance-attributes
     def upload_to_rekor(self):
         # tar_file = os.path.join(self.self.results_dir_path, 'results_file.tar')
         # sig_file = os.path.join(self.results_file_path, 'results_file.tar.asc')
-        artifact_file = os.path.join(self.results_dir_path, self.results_file_path)
+        artifact_file = self.results_file_path
         sig_file = artifact_file + '.asc'
         # tar = subprocess.run(['tar', '-cvf', tar_file, self.results_file_path],
         #                      stdout=subprocess.PIPE, universal_newlines=True)
@@ -467,7 +467,7 @@ class StepImplementer(ABC):  # pylint: disable=too-many-instance-attributes
                                 '--artifact',
                                 artifact_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                universal_newlines=True)
-        print("Artifact file:" + artifact_file)
+        print("Artifact file: " + artifact_file)
         if rekor.returncode != 0:
             return rekor.stderr
         return rekor.stdout
