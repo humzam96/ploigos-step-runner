3596bc3701af2507f8ff6fbc99495270a0e9f8e7
Humza Mohammad <humzam96@icloud.com>
2021-05-05 22:10:20 -0400

    fixing sig_file

--- src/ploigos_step_runner/step_implementer.py
+++ src/ploigos_step_runner/step_implementer.py
@@ -517,7 +517,7 @@ class StepImplementer(ABC):  # pylint: disable=too-many-instance-attributes
                               artifact_file], stdout=subprocess.PIPE, universal_newlines=True
                              )
         artifact_file_path = Path(os.path.realpath(artifact_file))
-        rekor_entry = self.create_rekor_entry(artifact_file_path,'/var/pgp-private-keys/gpg_public_key','sig_file')
+        rekor_entry = self.create_rekor_entry(artifact_file_path,'/var/pgp-private-keys/gpg_public_key',sig_file)
         rekor_entry_path = Path(os.path.join(self.work_dir_path, 'entry.json'))
         if rekor_entry_path.exists():
             rekor_entry_path.unlink()
