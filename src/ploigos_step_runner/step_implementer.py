35a3af604c276bc4416dc7c09710e7e7d4665c63
Humza Mohammad <humzam96@icloud.com>
2021-05-05 22:48:47 -0400

    Print rekor entry

--- src/ploigos_step_runner/step_implementer.py
+++ src/ploigos_step_runner/step_implementer.py
@@ -538,7 +538,7 @@ class StepImplementer(ABC):  # pylint: disable=too-many-instance-attributes
                                 rekor_entry_path.absolute()
                                 ], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                universal_newlines=True)
-        print("Rekor Entry: " +  rekor_entry)
+        print("Rekor Entry: " +  json.dumps(rekor_entry, indent = 4))
         if rekor.returncode != 0:
             return rekor.stderr
         return rekor.stdout
